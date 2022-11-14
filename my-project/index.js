b8ccdb56e222d6b7b3d9d9e5e1cf1c5cbf0e4605
doubleemm <mike.majunke@gmail.com>
2022-11-14 12:36:00 +0100

    added function handleRequest

--- my-project/index.js
+++ my-project/index.js
@@ -1,9 +1,33 @@
-export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+
+  async function readRequestBody(request) {
+	const { headers } = request;
+	const contentType = headers.get('content-type') || '';
+  
+	if (contentType.includes('application/json')) {
+	  return JSON.stringify(await request.json());
+	} else if (contentType.includes('application/text')) {
+	  return request.text();
+	} else if (contentType.includes('text/html')) {
+	  return request.text();
+	} else {
+	  return 'unspecified in header';
+	}
+  }
+  
+
+  async function handleRequest(request) {
+	const reqBody = await readRequestBody(request);
+	const retBody = `The request body sent in was ${reqBody}`;
+	return new Response(retBody);
+  }
+  
+  addEventListener('fetch', event => {
+	const { request } = event;
+	const { url } = request;
+  
+	if (request.method === 'POST') {
+	  return event.respondWith(handleRequest(request));
+	} else if (request.method === 'GET') {
+	  return event.respondWith(new Response(`The request was a GET`));
+	}
+  });
